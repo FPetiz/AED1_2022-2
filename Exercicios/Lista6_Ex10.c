1c7cbe57f176afa6547fe401b5398bb08f08c220
FPetiz <adnanref.c.petiz@gmail.com>
2023-03-02 18:21:02 -0300

    Initial commit

---
+++ Exercicios/Lista6_Ex10.c
@@ -0,0 +1,2 @@
+/*  10. Utilizando uma estrutura, fac¸a um programa que permita a entrada de nome, enderec¸o e
+    telefone de 5 pessoas e os imprima em ordem alfabetica. */
---
+++ Exercicios/Lista6_Ex23.c
@@ -0,0 +1,8 @@
+/*  23. Escreva um programa que receba dois structs do tipo dma, cada um representando uma
+data valida, e calcule o n ´ umero de dias que decorreram entre as duas datas. ´
+struct dma {
+int dia;
+int mes;
+int ano;
+};
+*/
